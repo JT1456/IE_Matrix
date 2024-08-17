d519348adc7db4fa797ee00e43ad1df5f017c6f1
JT1456 <jeddathomas@outlook.com>
2024-08-18 09:05:58 +1000

    Added task creation to homepage

--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 import plotly.express as px
-import plotly.graph_objects as go
+
 
 # Create your views here.
 def matrix(request):
 
+    # Handle task submissions
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('matrix')
+
+    else:
+        form = TaskForm()
+
     tasks = Task.objects.all()
 
-    #Prep data for plotting
+    # Prep data for plotting
     task_data = {
         'Title': [task.title for task in tasks],
         'Impact': [task.impact for task in tasks],
@@ -17,7 +28,7 @@ def matrix(request):
         'URL': [f"/task/{task.pk}/" for task in tasks]
     }
 
-    #Create scatter plot
+    # Create scatter plot
     fig = px.scatter(
         task_data,
         x="Impact",
@@ -34,6 +45,22 @@ def matrix(request):
         height=600
     )
 
+    # Limit axis
+    fig.update_layout(
+        shapes=[
+            dict(
+                type='line', x0=5, x1=5, y0=0, y1=10,
+                line=dict(color='Black', dash='dash')
+            ),
+            dict(
+                type='line', x0=0, x1=10, y0=5, y1=5,
+                line=dict(color='Black', dash='dash')
+            )
+        ],
+        xaxis=dict(range=[0, 10.1]),
+        yaxis=dict(range=[0, 10.1]),
+    )
+
     # Make tasks clickable
     fig.update_traces(
         marker=dict(size=12, opacity=0.8),
@@ -48,4 +75,5 @@ def matrix(request):
 
     graph_html = fig.to_html(full_html=False)
 
-    return render(request, 'tasks/matrix.html', {'graph': graph_html})
+
+    return render(request, 'tasks/matrix.html', {'graph': graph_html, 'form': form})
